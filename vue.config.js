aff4671ea99489f4d212815c53830dcc4f81502d
OptoMath <rachel.cunningham@logan.kyschools.us>
2020-08-24 23:34:55 -0500

    icons

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,34 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/optomath.github.io/" : "/"
+    process.env.NODE_ENV === "production" ? "/optomath.github.io/" : "/",
+    pwa: {
+      name: 'Opto Math',
+      themeColor: '#4DBA87',
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+      iconPaths: [{
+        "src": "img/icons/favicon-32x32.png",
+        "sizes": "32x32",
+        "type": "img/png"
+      },
+      {
+        "src": "img/icons/favicon-16x16.png",
+        "sizes": "16x16",
+        "type": "img/png"
+      },
+      {
+        "src": "img/icons/android-chrome-maskable-192x192.png",
+        "sizes": "192x192",
+        "type": "img/png",
+        "purpose": "any maskable"
+      },
+      {
+        "src": "img/icons/android-chrome-maskable-512x512.png",
+        "sizes": "512x512",
+        "type": "img/png",
+        "purpose": "any maskable"
+      }
+    ]
+    }
 };
