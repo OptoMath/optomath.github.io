eec2bf849b65de9e2880e6974329d353f236844c
OptoMath <rachel.cunningham@logan.kyschools.us>
2020-08-25 21:14:13 -0500

    links

--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,38 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production" ? "/optomath.github.io/" : "/",
-    pwa: {
-      name: 'Opto Math',
-      themeColor: '#0000FC',
-      msTileColor: '#000000',
-      appleMobileWebAppCapable: 'yes',
-      appleMobileWebAppStatusBarStyle: 'black',
-    manifestOptions: {
-      background_color: "#0000FC",
-      icons: [{
-        "src": "img/icons/favicon-32x32.png",
-        "sizes": "32x32",
-        "type": "img/png"
+  pwa: {
+    name: "Opto Math",
+    themeColor: "#0000FC",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    iconPaths: [
+      {
+        src: "img/icons/favicon-32x32.png",
+        sizes: "32x32",
+        type: "img/png"
       },
       {
-        "src": "img/icons/favicon-16x16.png",
-        "sizes": "16x16",
-        "type": "img/png"
+        src: "img/icons/favicon-16x16.png",
+        sizes: "16x16",
+        type: "img/png"
       },
       {
-        "src": "img/icons/android-chrome-maskable-192x192.png",
-        "sizes": "192x192",
-        "type": "img/png",
-        "purpose": "maskable"
+        src: "img/icons/android-chrome-maskable-192x192.png",
+        sizes: "192x192",
+        type: "img/png",
+        purpose: "maskable"
       },
       {
-        "src": "img/icons/android-chrome-maskable-512x512.png",
-        "sizes": "512x512",
-        "type": "img/png",
-        "purpose": "maskable"
+        src: "img/icons/android-chrome-maskable-512x512.png",
+        sizes: "512x512",
+        type: "img/png",
+        purpose: "maskable"
       }
-    ]
+    ],
+    manifestOptions: {
+      background_color: "#0000FC"
     }
-    }
+  }
 };
