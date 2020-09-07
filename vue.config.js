4c2a408649064b4c1d1d6bc8f0838286e1bca582
renovatioartes <renovatioartes@gmail.com>
2020-09-07 18:23:17 -0500

    Commit to master

--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,36 @@
 module.exports = {
   publicPath: "/",
   pwa: {
-    name: "Opto Math",
-    themeColor: "#0000FC",
-    msTileColor: "#000000",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black",
+    name: 'Opto Math',
+    themeColor: '#B8E986',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
     iconPaths: [
       {
-        src: "img/icons/favicon-32x32.png",
-        sizes: "32x32",
-        type: "img/png"
+        src: 'img/icons/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'img/png'
       },
       {
-        src: "img/icons/favicon-16x16.png",
-        sizes: "16x16",
-        type: "img/png"
+        src: 'img/icons/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'img/png'
       },
       {
-        src: "img/icons/android-chrome-maskable-192x192.png",
-        sizes: "192x192",
-        type: "img/png",
-        purpose: "maskable"
+        src: 'img/icons/android-chrome-maskable-192x192.png',
+        sizes: '192x192',
+        type: 'img/png',
+        purpose: 'maskable'
       },
       {
-        src: "img/icons/android-chrome-maskable-512x512.png",
-        sizes: "512x512",
-        type: "img/png",
-        purpose: "maskable"
+        src: 'img/icons/android-chrome-maskable-512x512.png',
+        sizes: '512x512',
+        type: 'img/png',
+        purpose: 'maskable'
       }
     ],
     manifestOptions: {
-      background_color: "#0000FC"
+      background_color: '#0000FC'
     }
   },
   transpileDependencies: ["vuetify"]
