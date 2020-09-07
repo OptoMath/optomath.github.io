6f5568df56347dd8cbc8764baff23b8e608e2500
renovatioartes <renovatioartes@gmail.com>
2020-09-07 18:41:45 -0500

    gh-pages

---
+++ precache-manifest.8924fbf90af1b61e2b825f2bd521d3cb.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88524c7b76513694f5e2",
+    "url": "/css/The Lesson Details.1d51adb6.css"
+  },
+  {
+    "revision": "dfe5f24be5f1cd5e9121",
+    "url": "/css/The Subject Units.26d3eb37.css"
+  },
+  {
+    "revision": "769d169fdb12696300d6",
+    "url": "/css/The Unit Lessons.26d3eb37.css"
+  },
+  {
+    "revision": "762956ce7924e8191001",
+    "url": "/css/app.609c7038.css"
+  },
+  {
+    "revision": "49000356e2b392ee0081",
+    "url": "/css/chunk-vendors.60551bed.css"
+  },
+  {
+    "revision": "8dfeaf52e44890b71fe651bd2aeb3c3c",
+    "url": "/fonts/Montebello-Rounded.8dfeaf52.woff"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/fonts/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/fonts/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/fonts/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/fonts/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/fonts/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/fonts/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/fonts/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/fonts/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/fonts/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/fonts/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/fonts/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/fonts/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "4c810be9a6af9d41571913a71cf0c677",
+    "url": "/img/avatar.4c810be9.svg"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/img/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/img/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/img/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "2c31ce7543535a7ffbc059296e9c219d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88524c7b76513694f5e2",
+    "url": "/js/The Lesson Details.64967586.js"
+  },
+  {
+    "revision": "dfe5f24be5f1cd5e9121",
+    "url": "/js/The Subject Units.3589760b.js"
+  },
+  {
+    "revision": "769d169fdb12696300d6",
+    "url": "/js/The Unit Lessons.33e030d2.js"
+  },
+  {
+    "revision": "762956ce7924e8191001",
+    "url": "/js/app.ad8cdc44.js"
+  },
+  {
+    "revision": "49000356e2b392ee0081",
+    "url": "/js/chunk-vendors.77ec1f4b.js"
+  },
+  {
+    "revision": "47ed5c030e382ac45e43a309f6718913",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
